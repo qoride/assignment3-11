4d33eb94f2bd3ba982f0cdb08081fd1ac04946a4
qoride <qoride@gmail.com>
2024-09-16 06:06:20 +0000

    main 5 commit

--- main5.cpp
+++ main5.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+//I want you to find the size and limits of short and double data types on your machine.
+
 using namespace std;
 
 int main(){
+    short shortVal;
+    double doubleVal;
+
+    cout << "Size of short is: " << sizeof(shortVal) << " bytes\n"
+    << "Size of double is: " << sizeof(doubleVal) << " bytes"
+    ;
 
     return 0;
 }
