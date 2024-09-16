1c83035468025f4c220705951bc9e38053fccef0
qoride <qoride@gmail.com>
2024-09-16 06:01:40 +0000

    main 4 commit

--- main4.cpp
+++ main4.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <iomanip>
+
+//I want you to program to print a table that have two rows and two columns,“course ” and “student” are the title.
+//Courses are C++ and JavaScript. And the number student for each are 100 and 50. our text labels are left align, and our numbers are right aligned.
 
 using namespace std;
 
 int main(){
-
+    cout << setw(20) << left << "Courses" << right << "Students\n"
+    << setw(20) << left << "C++" << right << "100\n"
+    << setw(20) << left << "Javascript" << right << "50"
+    ;
     return 0;
 }
