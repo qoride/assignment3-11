2261b576b03839aed3cd9f4c02898fcb5782a325
qoride <qoride@gmail.com>
2024-09-16 06:19:28 +0000

    main 6 commit

--- main6.cpp
+++ main6.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
+// I want you to write a program that asks the user a series of questions, like street, city, state and zip code, and then prints the user's address using this format.
+//Street
+//City,state,zipcpde.
+
 using namespace std;
 
 int main(){
+    string street,city,state;
+    int zip;
 
+    cout << "Enter your street: ";
+    getline(cin, street);
+    cout << "\nEnter your city: ";
+    getline(cin, city);
+    cout << "\nEnter your state: ";
+    getline(cin, state);
+    cout << "\nEnter your zip code: ";
+    cin >> zip;
+
+    cout << "\n\nYour address:\n" << street << "\n" << city << ", " << state << " " << zip;
+    
     return 0;
 }
