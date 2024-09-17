bb7c7426aa20dbf1945eea94aaac86f6bf64ed3c
qoride <qoride@gmail.com>
2024-09-17 03:19:08 +0000

    main 10 commit

--- main10.cpp
+++ main10.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+//Write a program that calculates the commission.
+
 using namespace std;
 
 int main(){
+    const int min = 10000;
+    const int max = 15000;
+    int sales, rate = 10;
+
+    cout << "Enter sales: ";
+    cin >> sales;
+    if(sales >= min){rate += 5;}
+    if(sales > max){rate += 5;}
+    cout << "\n\nYour commission rate is: " << rate << "%";
 
     return 0;
 }
